20c51d3164eec5ab8bc6c32b444b3827774e9250
Nethmi Gamage <nirashagamage3@gmail.com>
2024-01-27 04:16:41 +0200

    Game Ready!!

--- src/Components/ResetButton.js
+++ src/Components/ResetButton.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./ResetButton.css"
-
+//to reset game for another round after it's over
 export const ResetButton = ({resetBoard}) => {
   return (
     <button className="reset-button" onClick={resetBoard}>RESET</button>
